256ec10deb6ba7168c7b187c332dc5d1bb8f1001
Bhanu Bhandari <bhanubhandari99@gmail.com>
2019-04-09 06:57:51 +0530

    Added working router

--- test_celery/run_tasks.py
+++ test_celery/run_tasks.py
@@ -1,32 +1,23 @@
-from .tasks import longtime_add
-from .tasks import longtime_add, username_password_generator, multiplication, matrix_multiplication
+from .tasks import longtime_add, return_prime, multiply
 import time
+choice = 0
+choice = int(input('Enter task choice: \n 1 for add \n 2 for prime checking \n 3 for multiplication'))
+if(choice == 1):
+    x = int(input('Enter number to be added '))
+    y = int(input('Enter number to be added'))
+    result = longtime_add.delay(x,y)
+    result.ready()
+    print('Task result: ', result.get())
+if(choice == 2):
+    x = int(input('Enter number to be checked: '))
+    result = return_prime.delay(x)
+    result.ready()
+    print('Task result: ', result.get())
+if(choice == 3):
+    x = int(input('Enter number to be multiplied: '))
+    y = int(input('Enter the other number to be multiplied'))
+    result = multiply.delay(x,y)
+    result.ready()
+    print('Task result: ', result.get())
 
-# TASK: get these variables dynamically from the form and the file
-# task = "addition"
-task = "userpass"
-x = 2
-y = 100000000000004
 
-if __name__ == '__main__':
-    if task == "addition":
-        result = longtime_add.delay(x,y)
-    elif task == "userpass":
-        result = username_password_generator.delay(x)
-    elif task == "matmul":
-        pass
-    elif task == "multiplication":
-        pass
-        
-
-    print(result)
-
-    # TASK: result.result doesn't return our tuple in userpass. It returns 'test_celery.tasks.username_password_generator'. Why?
-
-    print("Task finished? " + str(result.ready()))
-    print("Task result: " + str(result.result))
-    time.sleep(10)
-    print("Task finished? " + str(result.ready()))
-    print("Task result: " + str(result.result))
-    print(type(result.result))
-    print(type(result))
