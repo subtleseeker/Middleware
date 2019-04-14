a8eb5d14494fdd16f3b2ca7cdd8bbb0431d50b13
captainCod3 <dwivedi.agam233@gmail.com>
2019-04-14 22:40:49 +0530

    File Uploads

--- flask_app/initial.py
+++ flask_app/initial.py
@@ -3,7 +3,7 @@ import os,subprocess
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = './Uploads'
-ALLOWED_EXTENSIONS = set(['txt', 'png', 'jpg', 'jpeg'])
+ALLOWED_EXTENSIONS = set(['txt', 'png', 'jpg', 'jpeg','pdf'])
 
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
@@ -21,6 +21,7 @@ def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
+            print("HIII")
             flash('No file attached!')
             return redirect(request.url)
         file = request.files['file']
@@ -30,14 +31,15 @@ def upload_file():
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
+            print("Hi")
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return "File uploaded successfully"
+            return redirect(url_for('upload_file',filename=filename))
     return render_template('index.html')  
 
-@app.route('/task', methods=['GET', 'POST'])
-def run_task():
-    subprocess.call('python -m ../test_celery.run_tasks')
+# @app.route('/task', methods=['GET', 'POST'])
+# def run_task():
+#     subprocess.call('python -m ../test_celery.run_tasks')
 
 if __name__=='__main__':
     app.secret_key='super secret key'
