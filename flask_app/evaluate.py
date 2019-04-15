57a029375391ef6b73c7a5ef0946b0b33f96f42c
Ruchin Agrawal <ruchinagarwal674@gmailcom>
2019-04-16 02:42:17 +0530

    initial.py

--- flask_app/evaluate.py
+++ flask_app/evaluate.py
@@ -1,14 +1,15 @@
-f = open("/home/gaddafi/sampl.txt", "r")
-line = f.readline()
-num = []
-while line:
-    b = line.rstrip('\n')
-    num.append(b)
-    line = f.readline()
-f.close()         
-num_ints = []
-for i in num:
-    x = [int(l) for l in i.split(" ")]
-    num_ints.append(x)
+#f = open("/home/ruchin/Distributed_system_technical/simple_flask_example/sample.txt", "r")
+#line = f.readline()
+#num = []
+#while line:
+#    b = line.rstrip('\n')
+#    num.append(b)
+#    line = f.readline()
+#f.close()         
+#num_ints = []
+#for i in num:
+#    x = [int(l) for l in i.split(" ")]
+#    num_ints.append(x)
 
-print(num_ints)    
+#print(num_ints)    
+print("yeyeye")
