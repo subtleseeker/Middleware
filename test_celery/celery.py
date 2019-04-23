9d84f4007dcfa33e63a13e1472e1d8b82919df3e
Nikhil Mundra <nikhilmundra29@gmail.com>
2019-04-23 23:43:39 +0530

    Multiple server implementation

--- test_celery/celery.py
+++ test_celery/celery.py
@@ -1,6 +1,10 @@
 from __future__ import absolute_import
 from celery import Celery
 
-app = Celery('test_celery', broker="amqp://bhanubb:bhanubhandari@localhost/bhanubhandari_vhost", backend="rpc://", include=['test_celery.tasks'])
+# app = Celery('test_celery', broker="amqp://bhanubb:bhanubhandari@localhost/bhanubhandari_vhost", backend="rpc://", include=['test_celery.tasks'])
 
 
+app = Celery('test_celery',
+			broker = 'amqp://username:password@localhost/subtleseeker_vhost',
+			backend= 'rpc://',
+			include=['test_celery.tasks'])
--- test_celery/tasks.py
+++ test_celery/tasks.py
@@ -4,19 +4,34 @@ import time
 from kombu import Queue
 import random
 
-app.conf.task_default_queue = 'default'
+app.conf.task_default_queue = 'celery'
 app.conf.tasks_queues = (
-    Queue('default', exchange ='default', routing_key='default'),
-    Queue('add', exchange='add', routing_key='add'),
-    Queue('multiply', exchange='multiply',routing_key='multiply'),
-    Queue('prime',exchange='prime',routing_key='prime')
+    Queue('default', exchange ='celery', routing_key='default'),
+    Queue('add', exchange='celery', routing_key='add'),
+    Queue('multiply', exchange='celery',routing_key='multiply'),
+    Queue('prime',exchange='celery',routing_key='prime'),
+    Queue('upgen',exchange='celery',routing_key='upgen')
+
 )
+
+# @app.task(queue='add')
+# def longtime_add(x, y):
+#     print('long time task begins')
+#     print ('long time task finished')
+#     l = []
+#     for i in range(5):
+#         k = x[i]+y[i]
+#         print("(((" ,k)
+#         l.append(k)
+
+#     return l
 
 @app.task(queue='add')
 def longtime_add(x, y):
     print('long time task begins')
     print ('long time task finished')
-    return x + y
+    return x+y
+
 
 @app.task(queue='prime')
 def return_prime(x):
@@ -31,7 +46,7 @@ def return_prime(x):
 def multiply(x, y):
     return x*y
 
-@app.task()
+@app.task(queue='upgen')
 def upgenerator():
     N = 3
     M = 5
@@ -53,3 +68,62 @@ def upgenerator():
                 username.append(s1)
                 password.append(s2)
     return (username, password)
+
+
+
+################## delete me ##################################
+# from __future__ import absolute_import
+# from test_celery.celery import app
+# import time
+# from kombu import Queue
+# import random
+
+# app.conf.task_default_queue = 'default'
+# app.conf.tasks_queues = (
+#     Queue('default', exchange ='default', routing_key='default'),
+#     Queue('add', exchange='add', routing_key='add'),
+#     Queue('multiply', exchange='multiply',routing_key='multiply'),
+#     Queue('prime',exchange='prime',routing_key='prime')
+# )
+
+# @app.task(queue='add')
+# def longtime_add(x, y):
+#     print('long time task begins')
+#     print ('long time task finished')
+#     return x + y
+
+# @app.task(queue='prime')
+# def return_prime(x):
+#     print('long time task begins')
+#     for i in range(2, x-1):
+#         if x % i == 0:
+#             return False
+#     else:
+#         return True
+
+# @app.task(queue='multiply')
+# def multiply(x, y):
+#     return x*y
+
+# @app.task()
+# def upgenerator():
+#     N = 3
+#     M = 5
+#     password = []
+#     username = []
+#     for x in range(N):
+#         s1 = ""
+#         s2 = ""
+        
+#         # print 10 random values
+#         # between 1 and 100
+#         for y in range(M):
+#             s1 = s1 + chr(random.randint(97, 122))
+#             z = random.randint(1,2)
+#             if(z  == 1):
+#                 s2 = s2 + chr(random.randint(97,122))
+#             else:
+#                 s2 = s2 + str(random.randint(1, 9))
+#                 username.append(s1)
+#                 password.append(s2)
+#     return (username, password)
