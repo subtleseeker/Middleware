929ad68a71490aaddcfe005d28f571d92fd811da
Nikhil Mundra <nikhilmundra29@gmail.com>
2019-04-27 10:34:24 +0530

    fault tolerance

--- test_celery/celery.py
+++ test_celery/celery.py
@@ -1,10 +1,25 @@
 from __future__ import absolute_import
 from celery import Celery
 
-# app = Celery('test_celery', broker="amqp://bhanubb:bhanubhandari@localhost/bhanubhandari_vhost", backend="rpc://", include=['test_celery.tasks'])
+# app = Celery('test_celery', 
+# 			broker="amqp://bhanubb:bhanubhandari@localhost/bhanubhandari_vhost", 
+# 			backend="rpc://", 
+# 			include=['test_celery.tasks'])
 
+
+# app = Celery('test_celery',
+# 			broker = 'amqp://username:password@localhost/subtleseeker_vhost',
+# 			backend= 'rpc://',
+# 			include=['test_celery.tasks'])
+
+# app = Celery('test_celery',
+# 			broker = 'amqp://agam1:agam123@172.26.42.99/agam1_vhost',
+# 			backend= 'rpc://',
+# 			include=['test_celery.tasks'])
 
 app = Celery('test_celery',
-			broker = 'amqp://username:password@localhost/subtleseeker_vhost',
+			broker = 'amqp://ruchin:ruchin123@172.26.42.183/ruchin__vhost',
 			backend= 'rpc://',
 			include=['test_celery.tasks'])
+
+app.config_from_object('test_celery.celeryconfig')
